7b9ffc7dd92f56491464a435d505cd000d19da3f
Kenneth Barnett <kbarnett7@gmail.com>
2025-04-26 09:32:51 -0700

    Prototype tailwind css class generation from component HTML file.

--- src/components/layout/nav-menu/nav-menu.component.ts
+++ src/components/layout/nav-menu/nav-menu.component.ts
@@ -13,42 +13,42 @@ class NavMenuComponent extends HTMLElement {
         const shadow = this.attachShadow({ mode: "open" });
         const wrapper = document.createElement("div");
 
-        // this.htmlService.loadHTML(
-        //     "./components/layout/nav-menu/nav-menu.component.html",
-        //     wrapper
-        // );
+        this.htmlService.loadHTML(
+            "./components/layout/nav-menu/nav-menu.component.html",
+            wrapper
+        );
 
-        wrapper.innerHTML = `
-            <style>
-                .sidenav a {
-                    padding: 8px 8px 8px 32px;
-                    text-decoration: none;
-                    font-size: 25px;
-                    color: #818181;
-                    display: block;
-                    transition: 0.3s;
-                }
+        // wrapper.innerHTML = `
+        //     <style>
+        //         .sidenav a {
+        //             padding: 8px 8px 8px 32px;
+        //             text-decoration: none;
+        //             font-size: 25px;
+        //             color: #818181;
+        //             display: block;
+        //             transition: 0.3s;
+        //         }
 
-                .sidenav a:hover {
-                    color: #f1f1f1;
-                }
+        //         .sidenav a:hover {
+        //             color: #f1f1f1;
+        //         }
 
-                .sidenav .closebtn {
-                    position: absolute;
-                    top: 0;
-                    right: 25px;
-                    font-size: 36px;
-                    margin-left: 50px;
-                }
-            </style>
-            <div id="mySidenav" class="sidenav h-screen w-0 fixed z-1 top-0 left-0 bg-zinc-900 overflow-x-hidden duration-500 pt-16">
-                <a href="javascript:void(0)" class="closebtn" onclick="this.getRootNode().host.closeNav()"> &times; </a>
-                <a href="#">About</a>
-                <a href="#">Services</a>
-                <a href="#">Clients</a>
-                <a href="#">Contact</a>
-            </div>
-        `;
+        //         .sidenav .closebtn {
+        //             position: absolute;
+        //             top: 0;
+        //             right: 25px;
+        //             font-size: 36px;
+        //             margin-left: 50px;
+        //         }
+        //     </style>
+        //     <div id="mySidenav" class="sidenav h-screen w-0 fixed z-1 top-0 left-0 bg-zinc-900 overflow-x-hidden duration-500 pt-16">
+        //         <a href="javascript:void(0)" class="closebtn" onclick="this.getRootNode().host.closeNav()"> &times; </a>
+        //         <a href="#">About</a>
+        //         <a href="#">Services</a>
+        //         <a href="#">Clients</a>
+        //         <a href="#">Contact</a>
+        //     </div>
+        // `;
 
         // Attach the closeNav method to the close button
         // wrapper.addEventListener("click", (event) => {
