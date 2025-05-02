78553aa1050115de01f2b7161ccc6da0a1aaa074
Ken Barnett <kbarnett7@gmail.com>
2025-05-02 09:04:26 -0700

    Use a Map object instead of an anonymous object.

--- src/data-access/json-file-database.ts
+++ src/data-access/json-file-database.ts
@@ -6,19 +6,19 @@ import { IDatabase } from "./database-interface";
 import { DbSet } from "./db-set";
 
 export class JsonFileDatabase implements IDatabase {
-    private _dbSets: { key: string; value: DbSet<any> }[] = [];
+    private _dbSets: Map<string, DbSet<any>>;
 
     constructor() {
-        this._dbSets = [];
-        this._dbSets.push({ key: ArmorItem.name, value: new DbSet<ArmorItem>(armorData) });
-        this._dbSets.push({ key: EquipmentItem.name, value: new DbSet<EquipmentItem>(equipmentData) });
+        this._dbSets = new Map<string, DbSet<any>>();
+        this._dbSets.set(ArmorItem.name, new DbSet<ArmorItem>(armorData));
+        this._dbSets.set(EquipmentItem.name, new DbSet<EquipmentItem>(equipmentData));
     }
 
     getSet<T>(type: string): DbSet<T> {
-        const dbSet = this._dbSets.find((dbSet) => dbSet.key === type);
+        const dbSet = this._dbSets.get(type);
 
         if (dbSet) {
-            return dbSet.value as DbSet<T>;
+            return dbSet as DbSet<T>;
         }
 
         throw new Error(`DbSet of type ${type} not found.`);
