e4e90799fc53691fec4f344e708358234743bcf2
Ken Barnett <kbarnett7@gmail.com>
2025-05-09 09:51:45 -0700

    Fix centering dialog issue.

--- src/components/gear-list/gear-list.ts
+++ src/components/gear-list/gear-list.ts
@@ -161,21 +161,18 @@ export class GearListComponent extends BaseComponent {
 
     public onTableDataRowClick(gearItem: GearListItem) {
         AppLogger.instance.debug(`Row clicked for item id = ${gearItem.id} and category = ${gearItem.category}`);
-        // const dialog = this.shadow.querySelector("#gearItemDialog") as HTMLDialogElement;
-
-        // dialog.showModal();
 
         const dialog = this.shadow.querySelector("#gearItemDialog") as HTMLDivElement;
+
         dialog.classList.remove("hidden");
+        dialog.classList.add("flex");
     }
 
     public onCloseDialog(event: MouseEvent) {
-        // const dialog = this.shadow.querySelector("#gearItemDialog") as HTMLDialogElement;
-
-        // dialog.close();
-
         const dialog = this.shadow.querySelector("#gearItemDialog") as HTMLDivElement;
+
         dialog.classList.add("hidden");
+        dialog.classList.remove("flex");
     }
 
     public onTableHeaderClick(header: string) {
