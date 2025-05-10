b6c23c4340306f6dc7c2282ae54e7be83576eb14
Ken Barnett <kbarnett7@gmail.com>
2025-05-10 07:54:37 -0700

    Add styled icons to header bar.

--- src/components/layout/header-bar/header-bar.ts
+++ src/components/layout/header-bar/header-bar.ts
@@ -3,6 +3,8 @@ import { BaseComponent } from "../../base.component";
 import { EventBus } from "../../../lib/events/event-bus";
 import { AppEvent } from "../../../lib/events/app-event";
 import { UpdatePageTitleEvent } from "../../../lib/events/update-page-title-event";
+import { ChangePageEvent } from "../../../lib/events/change-page-event";
+import { PageRouterService } from "../../../lib/pages/page-router-service";
 
 export class HeaderBarComponent extends BaseComponent {
     constructor() {
@@ -20,6 +22,12 @@ export class HeaderBarComponent extends BaseComponent {
     private updatePageTitle(updatedTitle: string) {
         const pageTitleElement = this.shadow.querySelector("#pageTitle") as HTMLHeadElement;
         pageTitleElement.textContent = updatedTitle;
+    }
+
+    private navigateToPage(page: string) {
+        const changePageEvent = new ChangePageEvent(PageRouterService.instance.getPageByTitle(page));
+
+        EventBus.instance.dispatch(changePageEvent);
     }
 }
 
