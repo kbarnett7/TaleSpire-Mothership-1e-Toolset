f505a1c9c159e9acf1ad4bbc9fde6d2f9c1111de
Ken Barnett <kbarnett7@gmail.com>
2025-05-09 18:29:05 -0700

    Add armor-specific fields to form.

--- src/components/gear-armor-form/gear-armor-form.ts
+++ src/components/gear-armor-form/gear-armor-form.ts
@@ -15,12 +15,16 @@ export class GearArmorFormComponent extends BaseComponent {
         this.render(html);
     }
 
-    public setEquipmentItem(equipmentItem: ArmorItem) {
-        this.armorItem = equipmentItem;
+    public setEquipmentItem(armorItem: ArmorItem) {
+        this.armorItem = armorItem;
 
         this.updateGearName();
         this.updateGearDescription();
         this.updateGearCost();
+        this.updateGearArmorPoints();
+        this.updateGearOxygen();
+        this.updateGearSpeed();
+        this.updateGearSpecial();
     }
 
     private updateGearName() {
@@ -36,6 +40,31 @@ export class GearArmorFormComponent extends BaseComponent {
     private updateGearCost() {
         const paragraph = this.shadow.querySelector("#gearCost") as HTMLParagraphElement;
         paragraph.textContent = CreditsAbbreviator.instance.abbreviate(this.armorItem.cost);
+    }
+
+    private updateGearArmorPoints() {
+        const paragraph = this.shadow.querySelector("#gearArmorPoints") as HTMLParagraphElement;
+        paragraph.textContent = this.armorItem.armorPoints.toString();
+    }
+
+    private updateGearOxygen() {
+        const paragraph = this.shadow.querySelector("#gearOxygen") as HTMLParagraphElement;
+
+        if (this.armorItem.oxygen === 0) {
+            paragraph.textContent = "None";
+        } else {
+            paragraph.textContent = `${this.armorItem.oxygen} hours`;
+        }
+    }
+
+    private updateGearSpeed() {
+        const paragraph = this.shadow.querySelector("#gearSpeed") as HTMLParagraphElement;
+        paragraph.textContent = this.armorItem.speed;
+    }
+
+    private updateGearSpecial() {
+        const paragraph = this.shadow.querySelector("#gearSpecial") as HTMLParagraphElement;
+        paragraph.textContent = this.armorItem.special;
     }
 }
 
