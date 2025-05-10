694c882be4803c12845f48b957cced8e7ff41b34
Ken Barnett <kbarnett7@gmail.com>
2025-05-10 14:54:39 -0700

    Add easy ways of closing the more options menu.

--- src/components/layout/footer-bar/footer-bar.ts
+++ src/components/layout/footer-bar/footer-bar.ts
@@ -17,11 +17,32 @@ export class FooterBarComponent extends BaseComponent {
         const changePageEvent = new ChangePageEvent(PageRouterService.instance.getPageByTitle(page));
 
         EventBus.instance.dispatch(changePageEvent);
+
+        this.closeMoreOptionsMenu();
+    }
+
+    private onMoreOptionsButtonClick(event: MouseEvent) {
+        if (this.isMoreOptionsMenuOpen()) {
+            this.closeMoreOptionsMenu();
+        } else {
+            this.openMoreOptionsMenu();
+        }
+    }
+
+    private isMoreOptionsMenuOpen(): boolean {
+        const moreOptionsMenu = this.shadow.querySelector("#moreOptionsMenu") as HTMLDivElement;
+
+        return !moreOptionsMenu.classList.contains("hidden");
     }
 
     private openMoreOptionsMenu() {
         const moreOptionsMenu = this.shadow.querySelector("#moreOptionsMenu") as HTMLDivElement;
         moreOptionsMenu.classList.remove("hidden");
+    }
+
+    private closeMoreOptionsMenu() {
+        const moreOptionsMenu = this.shadow.querySelector("#moreOptionsMenu") as HTMLDivElement;
+        moreOptionsMenu.classList.add("hidden");
     }
 }
 
