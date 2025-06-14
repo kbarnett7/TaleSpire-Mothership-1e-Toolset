ea41d886a3a4ddb249ffb687ad60d72cf132dca4
Ken Barnett <kbarnett7@gmail.com>
2025-06-14 08:45:24 -0700

    Add remaining fields to NPC form.

--- src/components/npc-form/npc-form.ts
+++ src/components/npc-form/npc-form.ts
@@ -19,6 +19,13 @@ export class NpcFormComponent extends BaseComponent {
 
         this.updateNpcName();
         this.updateNpcCombat();
+        this.updateInstinct();
+        this.updateHealth();
+        this.updateMaximumWounds();
+        this.updateArmorPoints();
+        this.updateAttacks();
+        this.updateSpecialAbilities();
+        this.updateDescription();
     }
 
     private updateNpcName() {
@@ -29,6 +36,61 @@ export class NpcFormComponent extends BaseComponent {
     private updateNpcCombat() {
         const paragraph = this.shadow.querySelector("#npcCombat") as HTMLParagraphElement;
         paragraph.textContent = this.npc.combat.toString();
+    }
+
+    private updateInstinct() {
+        const paragraph = this.shadow.querySelector("#npcInstinct") as HTMLParagraphElement;
+        paragraph.textContent = this.npc.instinct.toString();
+    }
+
+    private updateHealth() {
+        const paragraph = this.shadow.querySelector("#npcHealth") as HTMLParagraphElement;
+        paragraph.textContent = this.npc.health.toString();
+    }
+
+    private updateMaximumWounds() {
+        const paragraph = this.shadow.querySelector("#npcMaximumWounds") as HTMLParagraphElement;
+        paragraph.textContent = this.npc.maximumWounds.toString();
+    }
+
+    private updateArmorPoints() {
+        const paragraph = this.shadow.querySelector("#npcArmorPoints") as HTMLParagraphElement;
+        paragraph.textContent = this.npc.armorPoints.toString();
+    }
+
+    private updateAttacks() {
+        const container = this.shadow.querySelector("#npcAttacksContainer") as HTMLDivElement;
+
+        container.innerHTML = "";
+
+        for (var attack of this.npc.attacks) {
+            const paragraph = document.createElement("p");
+
+            paragraph.className = "text-base leading-relaxed text-black";
+            paragraph.textContent = `${attack.name}: ${attack.effect}`;
+
+            container.appendChild(paragraph);
+        }
+    }
+
+    private updateSpecialAbilities() {
+        const container = this.shadow.querySelector("#npcSpecialAbilitiesContainer") as HTMLDivElement;
+
+        container.innerHTML = "";
+
+        for (var ability of this.npc.specialAbilities) {
+            const paragraph = document.createElement("p");
+
+            paragraph.className = "text-base leading-relaxed text-black";
+            paragraph.textContent = ability.description;
+
+            container.appendChild(paragraph);
+        }
+    }
+
+    private updateDescription() {
+        const paragraph = this.shadow.querySelector("#npcDescription") as HTMLParagraphElement;
+        paragraph.textContent = this.npc.description;
     }
 }
 
