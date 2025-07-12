a0af0901415960eeef6105d0a54180cb4637a38a
Ken Barnett <kbarnett7@gmail.com>
2025-07-12 08:33:37 -0700

    Implement TaleSpireBlobStorage.

--- src/lib/data-access/talespire-blob-storage.ts
+++ src/lib/data-access/talespire-blob-storage.ts
@@ -1,15 +1,33 @@
 import { IBlobStorage } from "../common/data-access/blob-storage-interface";
 
+declare const TS: any;
+
 export class TaleSpireBlobStorage implements IBlobStorage {
     public async getBlobAsync(key: string): Promise<string> {
-        //TS: return ts.localStorage.global.getBlob();
-        //getBlob() returns a promise. This was not documented.
-        throw new Error("Method not implemented.");
+        if (!TS) {
+            return "";
+        }
+
+        let blob = "";
+
+        TS.localStorage.global.getBlob().then((globalBlob: string) => {
+            blob = globalBlob;
+        });
+
+        if (!blob || blob === "") {
+            return "";
+        }
+
+        return blob;
     }
 
     public async setBlobAsync(key: string, value: string): Promise<void> {
-        //TS: ts.localStorage.global.setBlob(fileContents);
-        //setBlob() returns a promise. This was not documented.
-        throw new Error("Method not implemented.");
+        if (!TS) {
+            return;
+        }
+
+        TS.localStorage.global.setBlob(value).then(() => {
+            console.log("Database set!");
+        });
     }
 }
