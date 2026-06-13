bff8bec044cb3332e2dc703c90aba05f63753fb3
Ken Barnett <kbarnett7@gmail.com>
2026-06-13 12:57:56 -0700

    Update player sort tests to match other sort tests.

--- tests/pcs/sort-player-characters-list-feature.test.ts
+++ tests/pcs/sort-player-characters-list-feature.test.ts
@@ -7,6 +7,7 @@ import { UnitOfWork } from "../../src/lib/data-access/unit-of-work";
 import { DataAccessUtils } from "../data-access/data-access-utils";
 import { GetAllPlayerCharactersFeature } from "../../src/features/player-characters/get-all-player-characters/get-all-player-characters-feature";
 import { EmptyRequest } from "../../src/lib/common/features/empty-request";
+import { ErrorCode } from "../../src/lib/errors/error-code";
 
 describe("SortPlayerCharactersListFeature", () => {
     let feature: SortPlayerCharactersListFeature;
@@ -14,18 +15,100 @@ describe("SortPlayerCharactersListFeature", () => {
     let allPlayerCharacters: PlayerCharacterListItem[];
 
     beforeEach(async () => {
-        const dbContext = await DataAccessUtils.getInitializedDbContext();
-        const unitOfWork = new UnitOfWork(dbContext);
         feature = new SortPlayerCharactersListFeature();
         request = new SortPlayerCharactersListRequest();
+        allPlayerCharacters = await getAllPlayerCharacterListItems();
+    });
 
-        const getAllFeature = new GetAllPlayerCharactersFeature(unitOfWork);
-        allPlayerCharacters = getAllFeature.handle(new EmptyRequest());
+    it("When exception occurs returns a failure result with error information", () => {
+        // Arrange
+        request.playerCharacterListItems = allPlayerCharacters;
+        request.sortState = new SortState(SortPlayerCharactersListFeature.fieldName);
+
+        jest.spyOn(request.sortState, "direction", "get").mockImplementation(() => {
+            throw new Error("Mocked exception");
+        });
+
+        // Act
+        const result: Result<PlayerCharacterListItem[]> = feature.handle(request);
+
+        // Assert
+        expect(result.isFailure).toBe(true);
+        expect(result.value).not.toBeDefined();
+        expect(result.error).toBeDefined();
+        expect(result.error.code).toBe(ErrorCode.QueryError);
+        expect(result.error.description).toContain("Mocked exception");
+    });
+
+    it("By no field returns PC list items in same order as before", async () => {
+        // Arrange
+        request.playerCharacterListItems = allPlayerCharacters;
+
+        // Act
+        const result: Result<PlayerCharacterListItem[]> = feature.handle(request);
+
+        // Assert
+        expect(result.isSuccess).toBe(true);
+        expect(result.value).toBeDefined();
+        expect(result.value?.length).toBe(allPlayerCharacters.length);
+
+        const sortedPlayerCharacters = result.value ?? [];
+
+        for (let i = 0; i < sortedPlayerCharacters.length; i++) {
+            expect(sortedPlayerCharacters[i].id).toBe(allPlayerCharacters[i].id);
+        }
     });
 
     it("Returns the original list when field is invalid", () => {
         // Arrange
-        const sortState = new SortState("invalid-field");
+        request.playerCharacterListItems = allPlayerCharacters;
+        request.sortState = new SortState("invalid-field");
+
+        // Act
+        const result: Result<PlayerCharacterListItem[]> = feature.handle(request);
+
+        // Assert
+        expect(result.isSuccess).toBe(true);
+        expect(result.value).toBeDefined();
+        expect(result.value?.length).toBe(allPlayerCharacters.length);
+
+        const sortedPlayerCharacters = result.value ?? [];
+
+        for (let i = 0; i < sortedPlayerCharacters.length; i++) {
+            expect(sortedPlayerCharacters[i].id).toBe(allPlayerCharacters[i].id);
+        }
+    });
+
+    it('By any field in "None" direction returns PC list items sorted by id from lowest to highest', () => {
+        // Arrange
+        const firstElement = allPlayerCharacters.shift();
+        if (firstElement) allPlayerCharacters.push(firstElement);
+
+        request.playerCharacterListItems = allPlayerCharacters;
+        request.sortState = new SortState(SortPlayerCharactersListFeature.fieldName);
+
+        // Act
+        const result: Result<PlayerCharacterListItem[]> = feature.handle(request);
+
+        // Assert
+        expect(result.isSuccess).toBe(true);
+        expect(result.value).toBeDefined();
+        expect(result.value?.length).toBe(allPlayerCharacters.length);
+
+        const sortedPlayerCharacters = result.value ?? [];
+
+        for (let i = 1; i < sortedPlayerCharacters.length; i++) {
+            expect(sortedPlayerCharacters[i].id).toBeGreaterThanOrEqual(sortedPlayerCharacters[i - 1].id);
+        }
+    });
+
+    it('By id in "Ascending" direction returns PC list items sorted by id from lowest to highest', async () => {
+        // Arrange
+        const sortState = new SortState();
+        sortState.set(SortPlayerCharactersListFeature.fieldId);
+        const firstElement = allPlayerCharacters.shift();
+        if (firstElement) allPlayerCharacters.push(firstElement);
+
         request.playerCharacterListItems = allPlayerCharacters;
         request.sortState = sortState;
 
@@ -34,28 +117,43 @@ describe("SortPlayerCharactersListFeature", () => {
 
         // Assert
         expect(result.isSuccess).toBe(true);
-        expect(result.value).toEqual(allPlayerCharacters);
+        expect(result.value).toBeDefined();
+        expect(result.value?.length).toBe(allPlayerCharacters.length);
+
+        const sortedPlayerCharacters = result.value ?? [];
+
+        for (let i = 1; i < sortedPlayerCharacters.length; i++) {
+            expect(sortedPlayerCharacters[i].id).toBeGreaterThanOrEqual(sortedPlayerCharacters[i - 1].id);
+        }
     });
 
-    it("Sorts by id when SortDirection is None", () => {
+    it('By id in "Descending" direction returns PC list items sorted by id from highest to lowest', async () => {
         // Arrange
-        const sortState = new SortState(SortPlayerCharactersListFeature.fieldName);
-        // Reset to None by not setting (default is None before first click)
+        const sortState = new SortState();
+        sortState.set(SortPlayerCharactersListFeature.fieldId);
+        sortState.set(SortPlayerCharactersListFeature.fieldId);
+        const firstElement = allPlayerCharacters.shift();
+        if (firstElement) allPlayerCharacters.push(firstElement);
+
         request.playerCharacterListItems = allPlayerCharacters;
-        request.sortState = new SortState();
+        request.sortState = sortState;
 
         // Act
         const result: Result<PlayerCharacterListItem[]> = feature.handle(request);
 
         // Assert
         expect(result.isSuccess).toBe(true);
-        const pcs = result.value ?? [];
-        for (let i = 1; i < pcs.length; i++) {
-            expect(pcs[i].id).toBeGreaterThanOrEqual(pcs[i - 1].id);
+        expect(result.value).toBeDefined();
+        expect(result.value?.length).toBe(allPlayerCharacters.length);
+
+        const sortedPlayerCharacters = result.value ?? [];
+
+        for (let i = 1; i < sortedPlayerCharacters.length; i++) {
+            expect(sortedPlayerCharacters[i].id).toBeLessThanOrEqual(sortedPlayerCharacters[i - 1].id);
         }
     });
 
-    it("Sorts by name in Ascending order", () => {
+    it('By name in "Ascending" direction returns PC list items sorted by name in alphabetical order', () => {
         // Arrange
         const sortState = new SortState();
         sortState.set(SortPlayerCharactersListFeature.fieldName);
@@ -67,13 +165,19 @@ describe("SortPlayerCharactersListFeature", () => {
 
         // Assert
         expect(result.isSuccess).toBe(true);
-        const pcs = result.value ?? [];
-        for (let i = 1; i < pcs.length; i++) {
-            expect(pcs[i].name.toLowerCase() >= pcs[i - 1].name.toLowerCase()).toBe(true);
+        expect(result.value).toBeDefined();
+        expect(result.value?.length).toBe(allPlayerCharacters.length);
+
+        const sortedPlayerCharacters = result.value ?? [];
+
+        for (let i = 1; i < sortedPlayerCharacters.length; i++) {
+            expect(
+                sortedPlayerCharacters[i].name.toLowerCase() >= sortedPlayerCharacters[i - 1].name.toLowerCase(),
+            ).toBe(true);
         }
     });
 
-    it("Sorts by name in Descending order", () => {
+    it('By name in "Descending" direction returns PC list items sorted by name in reverse alphabetical order', () => {
         // Arrange
         const sortState = new SortState();
         sortState.set(SortPlayerCharactersListFeature.fieldName);
@@ -86,13 +190,19 @@ describe("SortPlayerCharactersListFeature", () => {
 
         // Assert
         expect(result.isSuccess).toBe(true);
-        const pcs = result.value ?? [];
-        for (let i = 1; i < pcs.length; i++) {
-            expect(pcs[i].name.toLowerCase() <= pcs[i - 1].name.toLowerCase()).toBe(true);
+        expect(result.value).toBeDefined();
+        expect(result.value?.length).toBe(allPlayerCharacters.length);
+
+        const sortedPlayerCharacters = result.value ?? [];
+
+        for (let i = 1; i < sortedPlayerCharacters.length; i++) {
+            expect(
+                sortedPlayerCharacters[i].name.toLowerCase() <= sortedPlayerCharacters[i - 1].name.toLowerCase(),
+            ).toBe(true);
         }
     });
 
-    it("Sorts by class in Ascending order", () => {
+    it('By class in "Ascending" direction returns PC list items sorted by name in alphabetical order', () => {
         // Arrange
         const sortState = new SortState();
         sortState.set(SortPlayerCharactersListFeature.fieldClass);
@@ -104,13 +214,20 @@ describe("SortPlayerCharactersListFeature", () => {
 
         // Assert
         expect(result.isSuccess).toBe(true);
-        const pcs = result.value ?? [];
-        for (let i = 1; i < pcs.length; i++) {
-            expect(pcs[i].characterClass.toLowerCase() >= pcs[i - 1].characterClass.toLowerCase()).toBe(true);
+        expect(result.value).toBeDefined();
+        expect(result.value?.length).toBe(allPlayerCharacters.length);
+
+        const sortedPlayerCharacters = result.value ?? [];
+
+        for (let i = 1; i < sortedPlayerCharacters.length; i++) {
+            expect(
+                sortedPlayerCharacters[i].characterClass.toLowerCase() >=
+                    sortedPlayerCharacters[i - 1].characterClass.toLowerCase(),
+            ).toBe(true);
         }
     });
 
-    it("Sorts by class in Descending order", () => {
+    it('By class in "Descending" direction returns PC list items sorted by name in reverse alphabetical order', () => {
         // Arrange
         const sortState = new SortState();
         sortState.set(SortPlayerCharactersListFeature.fieldClass);
@@ -123,29 +240,29 @@ describe("SortPlayerCharactersListFeature", () => {
 
         // Assert
         expect(result.isSuccess).toBe(true);
-        const pcs = result.value ?? [];
-        for (let i = 1; i < pcs.length; i++) {
-            expect(pcs[i].characterClass.toLowerCase() <= pcs[i - 1].characterClass.toLowerCase()).toBe(true);
+        expect(result.value).toBeDefined();
+        expect(result.value?.length).toBe(allPlayerCharacters.length);
+
+        const sortedPlayerCharacters = result.value ?? [];
+
+        for (let i = 1; i < sortedPlayerCharacters.length; i++) {
+            expect(
+                sortedPlayerCharacters[i].characterClass.toLowerCase() <=
+                    sortedPlayerCharacters[i - 1].characterClass.toLowerCase(),
+            ).toBe(true);
         }
     });
 
-    it("Returns failure result when an exception is thrown during sort", () => {
-        // Arrange
-        const sortState = new SortState();
-        sortState.set(SortPlayerCharactersListFeature.fieldName);
+    async function getAllPlayerCharacterListItems(): Promise<PlayerCharacterListItem[]> {
+        const filterFeature = await getAllPlayerCharactersFeature();
 
-        jest.spyOn(sortState, "direction", "get").mockImplementation(() => {
-            throw new Error("Test sort error");
-        });
+        return filterFeature.handle(new EmptyRequest()) ?? [];
+    }
 
-        request.playerCharacterListItems = allPlayerCharacters;
-        request.sortState = sortState;
+    async function getAllPlayerCharactersFeature(): Promise<GetAllPlayerCharactersFeature> {
+        const dbContext = await DataAccessUtils.getInitializedDbContext();
+        const unitOfWork = new UnitOfWork(dbContext);
 
-        // Act
-        const result: Result<PlayerCharacterListItem[]> = feature.handle(request);
-
-        // Assert
-        expect(result.isFailure).toBe(true);
-        expect(result.error.description).toContain("Test sort error");
-    });
+        return new GetAllPlayerCharactersFeature(unitOfWork);
+    }
 });
