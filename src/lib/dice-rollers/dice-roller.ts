e764f84d3e8dcf6e6d6c9b73ce5844bc607ef7b9
Ken Barnett <kbarnett7@gmail.com>
2026-09-26 13:39:21 -0700

    Add ability to roll multiple numbers fo the same dice.

--- src/lib/dice-rollers/dice-roller.ts
+++ src/lib/dice-rollers/dice-roller.ts
@@ -1,13 +1,26 @@
 export class DiceRoller {
-    public roll(sides?: number): number {
+    public roll(sides?: number, dice?: number): number {
+        let numberOfDice = 1;
+
         if (!sides) return 0;
 
         if (sides < 1) return 0;
 
+        if (dice && dice > 1) numberOfDice = Math.trunc(dice);
+
         const normalizedSides = Math.trunc(sides);
         const min = 1;
         const max = normalizedSides;
+        let aggregatedRolls = 0;
 
+        for (let index = 0; index < numberOfDice; index++) {
+            aggregatedRolls += this.getRandomNumberBetweenMinAndMaxInclusiveBoth(min, max);
+        }
+
+        return aggregatedRolls;
+    }
+
+    private getRandomNumberBetweenMinAndMaxInclusiveBoth(min: number, max: number): number {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
 }
--- tests/dice-roller/dice-roller.test.ts
+++ tests/dice-roller/dice-roller.test.ts
@@ -96,12 +96,81 @@ describe("DiceRoller", () => {
         }
     });
 
-    function rollDice(numberOfRolls: number, sides: number): number[] {
+    it("Should return 2 when rolling 2d1", () => {
+        // Arrange
+        // Act
+        const result = diceRoller.roll(1, 2);
+
+        // Assert
+        expect(result).toBe(2);
+    });
+
+    it("Should truncate number of dice greater than 1 with decimals", () => {
+        // Arrange
+        // Act
+        const result = diceRoller.roll(1, 2.5);
+
+        // Assert
+        expect(result).toBe(2);
+    });
+
+    it("Should roll a single dice when given a value between 0 and 1", () => {
+        // Arrange
+        // Act
+        const result = diceRoller.roll(1, 0.5);
+
+        // Assert
+        expect(result).toBe(1);
+    });
+
+    it("Should roll a single dice when given a value less than 0", () => {
+        // Arrange
+        // Act
+        const result = diceRoller.roll(1, -2);
+
+        // Assert
+        expect(result).toBe(1);
+    });
+
+    it("Should return all numbers between 2 and 12 within 600 rolls for a 2d6 dice", () => {
+        // Arrange
+        // Act
+        const results = rollDice(600, 6, 2);
+
+        // Assert
+        expect(results).toContain(2);
+        expect(results).toContain(3);
+        expect(results).toContain(4);
+        expect(results).toContain(5);
+        expect(results).toContain(6);
+        expect(results).toContain(7);
+        expect(results).toContain(8);
+        expect(results).toContain(9);
+        expect(results).toContain(10);
+        expect(results).toContain(11);
+        expect(results).toContain(12);
+    });
+
+    it("Should not contain any values less than 2 and greater than 12 within 600 rolls for a 2d6 dice", () => {
+        // Arrange
+        // Act
+        const results = rollDice(600, 6, 2);
+
+        // Assert
+        for (const result of results) {
+            expect(result).toBeGreaterThanOrEqual(2);
+            expect(result).toBeLessThanOrEqual(12);
+        }
+    });
+
+    function rollDice(numberOfRolls: number, sides: number, dice?: number): number[] {
         let results: number[] = [];
+        let numberOfDice = dice ?? 1;
 
         for (let index = 0; index < numberOfRolls; index++) {
-            results.push(diceRoller.roll(sides));
+            results.push(diceRoller.roll(sides, numberOfDice));
         }
+
         return results;
     }
 });
